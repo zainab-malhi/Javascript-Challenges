0b64fd1ebfe95dc1f8803e72ffedae8e45e0bd37
zainab <zainab.malhi@outlook.com>
2024-02-12 13:57:06 -0500

    commiting challenge 5

--- Challenge5/script.js
+++ Challenge5/script.js
@@ -1,26 +1,50 @@
-//challenge (4):
+// Challenge(5) - Variables.
 
-//1. Create a button with the text 'Click' inside.
+// 5.a. Create a <script> element. Inside the <script>, create a variable called "name", and save your name in this variable (as a string).
+let name = "zainab";
 
-//2. Create 2 buttons 1 with your name and 1 with your favorite food.
+// 5.b. Continuing from 5a, display the message "My name is: ${name}" in the console (insert the 'name' variable created in 5a into this message).
+console.log(`My name is ${name}`);
 
-//3. Create a paragraph with the text "Hello, World!" inside.
+// 5.c. At a restraurant, you order 1 coffee ($5), 2 bagals ($3 each), and 1 soup ($9). Calculate the cost and save it in a variable called 'cost'.
+let coffeePrice = 5;
+let bagelPrice = 3;
+let soupPrice = 9;
 
-//4. Continuing from 3rd qos, create a paragraph below the prevois paragraph and write something you did today.
+let coffeeQuantity = 1;
+let bagelsQuantity = 2;
+let soupQuantity = 1;
 
-//5. Create 2 buttons 'Add to cart' and Buy now". Use CSS to give them yellow and orange background colors.
+let cost =
+  coffeePrice * coffeeQuantity +
+  bagelPrice * bagelsQuantity +
+  soupPrice * soupQuantity;
 
-//6. Change the title of tab to "Buttons"
+// 5.d. Continuing from 5c, display "cost of food: $${cost}" in the console.
 
-//7. Add a script element in HTML so that when the page loads, it displays the message: "Welcome" in the console and as a popup().
+console.log(`cost of food ${cost}`);
 
-//8. Using HTML and CSS create this template:
+// 5.e. Let's say the restaurance charges a 10% tax. Using the 'cost' variable from 5c, calculate the tax (hint: multiply by 0.1), and save the result in a variable.
 
-//Adults Plain Cotton T-Shirt
-//Price: $7.99
+let tax = 10 / 100;
+let taxAmount = cost * tax;
 
-//Add to Cart (button) | Buy Now (button)
+// 5.f Continuing from 5e, display "Tax (10%): $${tax}" in the console.
+console.log(`Tax(10%): ${taxAmount}`);
 
-// 9. also in qos (8) add the onclick="..." attribute to the two buttons:
-// 9.1) When clicking "Add to cart" create a popup with the text "Added".
-//9.2) When clicking "Buy now", display the message "Loading" in the Console, and then create a popup with the text "Purchased".
+// 5.g. Continuing from 5f, calculate the total (cost+tax), save it in a variable called "totalcost", and display the message "Total cost: $${totalcost}" in the console.
+let totalCost = cost + taxAmount;
+console.log(`Total cost: ${totalCost}`);
+
+// 5.h. In the cart quantity project, add 2 more buttons "+4" and "+5", which increases the quantity by 4 and 5 when you click them. Try using +=
+//Which cart project it is talking about??
+
+// 5.i. In the cart quantity project, add a button "remove from cart", which decreases the cart quantity by 1.
+//Which cart project it is talking about??
+
+// 5.j. Add 2 buttons '-2' and '-3', which decrease the quantity by 2 and 3.
+//Which cart project it is talking about??
+
+// 5.k. Creating a basic calculator that can perform very simple steps: like
+// 1+2=3 (where 1,2,+ are the buttons)
+//imcomplete description of challenge
